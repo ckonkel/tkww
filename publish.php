8673324a03bb05956b03e95ffd9b4dacf8eef7d1
Chris Konkel <>
2019-11-08 13:25:37 -0600

    added logic and dependency to gracefully pasrse json, added additional test payloads

--- publish.php
+++ publish.php
@@ -4,7 +4,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config/cloudMQTT.php';
 
 $service = new TKWW\Messaging\Services\CloudMQTT($config,"local-pub");
+$service->publish("test/data",'{"hello": 1 ] }');
 $service->publish("test/data",'{hello: "world"}');
 $service->publish("test/data",'{"hello": "world"}');
 $service->publish("test/data",'hello world');
-
+$service->publish("test/data",001);
+$service->publish("test/data","end");
+$service->publish("test/data","1234");
--- src/Services/CloudMQTT.php
+++ src/Services/CloudMQTT.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace TKWW\Messaging\Services;
 
 use Bluerhinos\phpMQTT;
+use Services_JSON;
 use TKWW\Messaging\ServiceInterface;
 
 class CloudMQTT implements ServiceInterface
@@ -57,11 +58,13 @@ class CloudMQTT implements ServiceInterface
             echo "Time out!\n";
             exit(1);
         }
+
         $topics[$topic] = array("qos" => 0, "function" => array($this, 'output'));
         $this->service->subscribe($topics, 0);
 
         while ($this->service->proc()) {
         }
+
         $this->service->close();
     }
 
@@ -71,9 +74,46 @@ class CloudMQTT implements ServiceInterface
      */
     public function output(string $topic, $msg): void
     {
+        $invalid_json_fixed_message = "Not required";
+        $final_msg = $msg;
+
+        $valid_json_found = $this->isJSON($msg);
+
+        $valid_json_initial_msg = json_last_error_msg();
+        $valid_json_initial_error = json_last_error();
+
+        if (!$valid_json_found) {
+            try {
+                $json = new Services_JSON(SERVICES_JSON_LOOSE_TYPE);
+                $fix_decoded_msg = $json->decode($msg);
+
+                if (!is_array($fix_decoded_msg)) {
+                    throw new \Exception("Could not fix gracefully");
+                }
+
+                $invalid_json_fixed_message = "Yes";
+                $final_msg = json_encode($fix_decoded_msg);
+            } catch (\Exception $e) {
+                $invalid_json_fixed_message = $e->getMessage();
+            }
+        }
+
+        $json_valid_message =
+            !$valid_json_found ? $valid_json_initial_msg . " - " . $valid_json_initial_error : $valid_json_initial_msg;
+
         echo "Received: " . date("r") . "\n";
+        echo "Valid: " . $json_valid_message . "\n";
+        echo "Fixed: " . $invalid_json_fixed_message . "\n";
         echo "Topic: {$topic}\n";
-        echo "Message:\n";
+        echo "Original Message:\n";
         echo $msg . "\n";
+        echo "Sent Message:\n";
+        echo $final_msg . "\n\n";
+    }
+
+    public function isJSON($value)
+    {
+        return !is_numeric($value) && is_array(json_decode($value, true))
+        && (json_last_error() == JSON_ERROR_NONE) ? true : false;
     }
 }
