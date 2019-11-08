459242a286cb8e6b9e79b42bb5dbdd4df65d1fbf
Chris Konkel <>
2019-11-08 13:23:01 -0600

    Fixed bug with using same client id to publish and subscribe to  messages

--- listen.php
+++ listen.php
@@ -1,7 +1,12 @@
 <?php
 
+error_reporting(E_ALL ^ E_DEPRECATED);
+
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config/cloudMQTT.php';
 
-$service = new TKWW\Messaging\Services\CloudMQTT($config, "local-pub");
+//PAIN IN THE ARSE DISCOVERY: YOU CAN NOT HAVE the same client id for the publisher and the subscriber...
+//$service = new TKWW\Messaging\Services\CloudMQTT($config, "local-pub");
+
+$service = new TKWW\Messaging\Services\CloudMQTT($config,"local-sub");
 $service->subscribe("test/data");
